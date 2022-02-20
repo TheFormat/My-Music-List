c9e1f90f02563950bbd2a8506b2d60a4aae5b373
TheFormat <95959211+TheFormat@users.noreply.github.com>
2022-02-20 13:56:42 +0900

    Update MyMusic.py

--- MyMusic.py
+++ MyMusic.py
@@ -1,10 +1,10 @@
-import random
+import random #include 랜덤이랑 같은 것임
 
 class MyMusic:
     def __init__(self, title, artist, rating):
         self.title = title
         self.artist = artist
-        self.rating = rating
+        self.rating = rating #생성자 뭐시기
 
     def Rec(self):
         self.rating += 1;
@@ -22,11 +22,11 @@ m = [
     MyMusic("In Your Eyes", "The Weeknd", 9),
     MyMusic("Boulevard of Broken Dreams", "Green Day", 7),
     MyMusic("Snow(Hey oh)", "Red Hot Chili Peppers", 9)
-]
+] # 리스트
 
-m[5].Rec()
+m[random.randrange(0, len(m))].Rec() #랜덤추천으로 바꿈, "리스트 안에 있는 요소"."함수" 이렇게 쓴다.->이것이 객체지향프로그래밍
 
-print("Today's song is \"" + m[random.randrange(0, len(m))].title + "\"" + "\n\n\n")
+print("Today's song is \"" + m[random.randrange(0, len(m))].title + "\"\n\n\n")
 
 for i in m:
     print(i.title + "\n" + str(i.rating) + "\n")
