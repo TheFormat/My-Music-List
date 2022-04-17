7e8fa8ea46358a27298b05102dd99a9ae1361a16
TheFormat <95959211+TheFormat@users.noreply.github.com>
2022-04-17 11:38:03 +0900

    Update MyMusic.cpp

--- MyMusic.cpp
+++ MyMusic.cpp
@@ -38,19 +38,27 @@ class EveryMusicList {
     
     public : 
         void setMusicList() {
-            addMusic("AHHA", "Nate Ruess", 8);
+            addMusic("AHHA", "Nate Ruess", 7);
             addMusic("Runaway", "Kanye West", 9);
-            addMusic("Grace Kelly", "MIKA", 7);
-            addMusic("Gansta's Paradise", "Coolio", 8);
-            addMusic("Sunday Candy", "Chance the Rapper", 8);
-            addMusic("Fuck You", "Cee Lo Green", 8);
-            addMusic("Summertime Sadness", "Lana Del Rey", 8);
-            addMusic("In Your Eyes", "The Weeknd", 9);
-            addMusic("Boulevard of Broken Dreams", "Green Day", 7);
-            addMusic("Snow(Hey oh)", "Red Hot Chili Peppers", 9);
-            addMusic("Kids", "MGMT", 8);
-            addMusic("Erase your Social", "Lil Uzi Vert", 8);
-            addMusic("I Miss You", "blink-182", 9);
+            addMusic("Grace Kelly", "MIKA", 4);
+            addMusic("Gansta's Paradise", "Coolio", 6);
+            addMusic("Sunday Candy", "Chance the Rapper", 6);
+            addMusic("Fuck You", "Cee Lo Green", 7);
+            addMusic("Summertime Sadness", "Lana Del Rey", 7);
+            addMusic("In Your Eyes", "The Weeknd", 8);
+            addMusic("Boulevard of Broken Dreams", "Green Day", 3);
+            addMusic("Snow(Hey oh)", "Red Hot Chili Peppers", 4);
+            addMusic("Kids", "MGMT", 6);
+            addMusic("Erase your Social", "Lil Uzi Vert", 7);
+            addMusic("I Miss You", "blink-182", 6);
+            addMusic("There's a good reason these tables are numbered", "Panic! at the disco", 7);
+            addMusic("Numb", "Linkin Park", 5);
+            addMusic("Finesse", "Bruno Mars", 6);
+            addMusic("Pursuit of Happiness", "Kid Cudi", 8);
+            addMusic("I Miss You", "blink-182", 6);
+            addMusic("La La Land", "Demi Lovato", 5);
+            addMusic("She Knows", "J.Cole", 6);
+            addMusic("Taco Tuesday", "Migos", 4);
         }
         
         void showMusicList() {
