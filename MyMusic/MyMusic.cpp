ed5c390e0bbace43f92ee7c6972b54799869d8cb
TheFormat <95959211+TheFormat@users.noreply.github.com>
2022-08-06 22:48:30 +0900

    보기좋게 순서 변경 근데 함수에 함수 용도 주석 달아논거 (void print(std::string line); // 출력 함수  <<<< 이런거) 지우면 좋을거같아요. 저정도면 설명이 따로 필요 없을정도로 잘 지어진 이름이에요

--- MyMusic/MyMusic.cpp
+++ MyMusic/MyMusic.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include "MyMusic.h"    // 보통 헤더파일을 구현하는 소스파일에서는 해당 헤더파일을 가장 위에 include 해줌
+
+#include <iostream>     // 그리고나서 보통 한줄 띄고 필요한 헤더들을 include 해줌
 #include <random>
-#include "MyMusic.h"
 
-using namespace std;
+using namespace std;    // cpp파일에서는 using namespace를 사용해도 헤더파일을 include하는 파일에서 영향을 안받기 때문에 해줘도 좋음 
 
+// 나는 using 구문 이후로 두줄정도 떼고 본문 작성하는 편임. include 부분이랑 본문이랑 구분감이 생겨서 좋은듯
 void print(string line) {
     printf("%s\n", line.c_str()); // printf하고 줄바꾸는 걸 하기 위해. 그리고 c_str은 c 스타일의 string이다.
 }
--- MyMusic/MyMusic.h
+++ MyMusic/MyMusic.h
@@ -3,6 +3,8 @@
 
 #include <string>
 
+// using namespace std;를 지운 이유는, main.cpp에서 이 파일을 include 하면 의도치 않게 using namespace std;를 사용한 꼴이 됨. 따라서 헤더파일에서는 해당 구문 사용 자제.
+
 void print(std::string line); // 출력 함수
 
 class MyMusic {
@@ -13,15 +15,10 @@ class MyMusic {
     
     public :
         void setInfo(std::string title, std::string artist, unsigned int rating); // 노래 설정
-        
         void showSong(); // 노래, 아티스트, 점수 보여주기
-        
         std::string getArtist(); // 아티스트 필터 getter
-        
         int getRating(); // 점수 필터 getter
-        
         void setArtist(std::string artist); // 아티스트 필터 setter
-        
         void setRating(unsigned int rating); // 점수 필터 setter
 };
 
@@ -35,13 +32,9 @@ class EveryMusicList {
     
     public : 
         void setMusicList(); // 노래 리스트 추가
-        
         void showMusicList(); // 노래 보여주기
-        
         void todayRec(); // 오늘의 추천
-        
         void filterArtist(std::string artist_); // 아티스트 검색
-        
         void filterRating(unsigned int rating_); // 점수 별 검색
 };
 
